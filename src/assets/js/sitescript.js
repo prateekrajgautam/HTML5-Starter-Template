50527c3d012246f8483466d4a6b27b02e18453b6
prateekrajgautam <prateekrajgautam@gmail.com>
2022-05-12 19:25:58 +0530

    update

--- src/assets/js/sitescript.js
+++ src/assets/js/sitescript.js
@@ -1,7 +1,19 @@
-// to toggle menu on click
-window.addEventListener("load", function () {
+// To toggle menu on click
+window.addEventListener("load", () => {
     var toggle = document.getElementsByClassName('toggle')[0]
     toggle.addEventListener("click", toggleItems)
+    var codeblocks = document.getElementsByClassName('code')
+    for (i = 0; i < codeblocks.length; i++) {
+        addCopyfunction(codeblocks[i])
+    }
+    codeblocks = document.getElementsByTagName('code')
+    for (i = 0; i < codeblocks.length; i++) {
+        addCopyfunction(codeblocks[i])
+    }
+    codeblocks = document.getElementsByTagName('pre')
+    for (i = 0; i < codeblocks.length; i++) {
+        addCopyfunction(codeblocks[i])
+    }
 
 })
 
@@ -15,4 +27,14 @@ function toggleItems() {
     }
 }
 
-;
+
+// Click to copy
+function addCopyfunction(c) {
+    // c.append(gen(span, '', "", 'copyIcon'))
+    c.addEventListener('click', (e) => {
+        var copyText = e.target.innerText
+        // console.log(copyText)
+        navigator.clipboard.writeText(copyText)
+
+    })
+}
