6e383c6efdf5738382b933cebff1dd1c71328b85
prateekrajgautam <prateekrajgautam@gmail.com>
2022-05-27 22:13:27 +0530

    update

--- src/002-HTML5-Starter-Template-with-Theme-Customizer/assets/js/colorconfig.js
+++ src/002-HTML5-Starter-Template-with-Theme-Customizer/assets/js/colorconfig.js
@@ -6,18 +6,21 @@ function cssVar(name, value) {
     if (value) document.documentElement.style.setProperty(name, value)
     return getComputedStyle(document.documentElement).getPropertyValue(name);
 }
+document.getElementById("bg1").value = cssVar("--hue")
 var elem = document.getElementById("bg1")
 elem.oninput = function () {
     var currentValue = this.value
     document.getElementById("bg1disp").innerHTML = currentValue
     cssVar("--hue", currentValue)
 }
+document.getElementById("bg2").value = cssVar("--hueComp")
 var elem = document.getElementById("bg2")
 elem.oninput = function () {
     var currentValue = this.value
     document.getElementById("bg2disp").innerHTML = currentValue
     cssVar("--hueComp", currentValue)
 }
+document.getElementById("bg3").value = cssVar("--hueAscent")
 var elem = document.getElementById("bg3")
 elem.oninput = function () {
     var currentValue = this.value
@@ -25,19 +28,23 @@ elem.oninput = function () {
     cssVar("--hueAscent", currentValue)
 }
 
-
+document.getElementById("text-color-light").value = cssVar("--huetextlight")
 var elem = document.getElementById("text-color-light")
 elem.oninput = function () {
     var currentValue = this.value
     document.getElementById("text-color-light-disp").innerHTML = currentValue
     cssVar("--huetextlight", currentValue)
 }
+
+document.getElementById("text-color-dark").value = cssVar("--huetextdark")
 var elem = document.getElementById("text-color-dark")
 elem.oninput = function () {
     var currentValue = this.value
     document.getElementById("text-color-dark-disp").innerHTML = currentValue
     cssVar("--huetextdark", currentValue)
 }
+
+document.getElementById("text-font-size").value = cssVar("--body-font-size") / 16.0
 var elem = document.getElementById("text-font-size")
 elem.oninput = function () {
     var currentValue = this.value
@@ -46,19 +53,28 @@ elem.oninput = function () {
     document.body.style.fontSize = currentValue * 16 + "px"
 }
 
-function closecustomizer() {
-    document.getElementById("Customizer").style.display = "none";
-    document.getElementById("app").style.gridTemplateColumns = '1fr'
-}
 
+// document.getElementById("FontSelect").value = cssVar("--font-h2")
 function changeBodyFont() {
     var fontselecteled = document.getElementById("FontSelect").value
-    console.log(fontselecteled)
     document.body.style.fontFamily = fontselecteled
     var all_headings = document.querySelectorAll("h1,h2,h3,h4,h5,h6")
     for (i = 0; i < all_headings.length; i++) {
         cssVar("--font-h2", `"${fontselecteled}";`)
         all_headings[i].style.fontFamily = fontselecteled
     }
-    console.log(all_headings)
+}
+
+function closecustomizer() {
+    document.getElementById("Customizer").style.display = "none";
+    document.getElementById("app").style.gridTemplateColumns = '1fr'
+    // document.getElementById("showCustomizer").onclick = "showCustomizer"
+
+}
+
+function showCustomizer() {
+    document.getElementById("Customizer").style.display = "inline-grid";
+    document.getElementById("app").style.gridTemplateColumns = '1fr 3fr'
+    // document.getElementById("showCustomizer").onclick = "closecustomizer()"
+
 }
